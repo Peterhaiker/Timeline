56d1321b0f17e09eb72477c512fc9b9cc246a98a
Peterhaiker <vim.memory@gmail.com>
2017-12-02 00:45:02 +0800

    add func_model directory

---
+++ func_model/login.c
@@ -0,0 +1,17 @@
+/*
+ * login.c
+ * Copyright (C) 2017-12-02 00:41 
+ * author  Peterhaiker 
+ * email   <vim.memory@gmail.com>
+ *
+ * description:
+ */
+
+#include "stdio.h"
+#include"../header/timeline.h"
+
+int login(void)
+{
+  puts("")
+  return 0;
+}
