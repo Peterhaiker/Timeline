61952dc2208a35164ec0fee481a5b3f6caf337ce
Peterhaiker <vim.memory@gmail.com>
2018-01-14 11:54:01 +0800

    完成登录模块，下一步让输入的密码不可视

--- func_model/login.c
+++ func_model/login.c
@@ -14,6 +14,8 @@
 
 void login(void)
 {
+  char name[20]={'\0'};
+  char passwd[50]={'\0'};
   char*sql=NULL;
   char dest[200]={'\0'};
   char ch;
@@ -24,13 +26,13 @@ void login(void)
   }
   while(1){
     //连接数据库成功
-    system("reset");
+show_menu:system("reset");
     puts("\t" Format_Double_Symbol);
     puts("\t||\t             Welcome to Timeline                  ||");
     puts("\t" Format_Double_Symbol);
-    puts("\t\t             1:login   2:sign up");
+    puts("\t\t         1:login   2:sign up   3:exit");
     printf("\t请选择:_\b");
-    while(1!=scanf("%[12]",&ch)){
+    while(1!=scanf("%[123]",&ch)){
       while('\n'!=getchar());
       printf("\t错误输入!!!请重新选择:_\b");
     }
@@ -39,8 +41,6 @@ void login(void)
     if('1'==ch){
       //登录
       printf("\t账户名:__\b\b");
-      char name[20]={'\0'};
-      char passwd[50]={'\0'};
       fgets(name,20,stdin);
       name[strlen(name)-1]='\0';
       printf("\t密码:__\b\b");
@@ -51,27 +51,85 @@ void login(void)
       strncpy(dest,sql,200);
       strncat(dest,passwd,200);
       strncat(dest,"')",200);
+      if(mysql_query(&mysql,dest)){
+        //sql语句执行失败
+        printf("\t服务器故障，请稍后再试!!!按回车继续...\n");
+        getchar();
+        //回收结果集空间
+        mysql_free_result(result);
+        exit(EXIT_FAILURE);
+      }
+      else{//sql语句执行成功
+        result=mysql_store_result(&mysql);
+        if(0!=mysql_num_rows(result))
+          //数据库存有账户数据
+          while((row=mysql_fetch_row(result)))
+            if(0==strcmp(name,row[0])){
+              printf("\t登录成功，2秒后自动跳转...\n");
+              goto show_profile;//如果匹配成功就跳转，否则即为输入错误
+            }
+        //登录错误或者数据库没有存有账户时
+        printf("\t账户名或密码输入错误!!!\n");
+        printf("\t按回车继续...\n");
+        getchar();
+        //回收结果集空间
+        mysql_free_result(result);
+      }
+    }
+    else if('2'==ch){
+      //注册
+      printf("\t账户名:_\b");
+      fgets(name,20,stdin);
+      name[strlen(name)-1]='\0';
+      //确定此用户名是否已存在
+      sql="select * from passwd where number='";
+      strncpy(dest,sql,200);
+      strncat(dest,name,200);
+      strncat(dest,"'",200);
       if(mysql_query(&mysql,dest))
-        //登录失败
+        //执行sql语句错误
         printf("\t服务器故障，请稍后再试!!!\n");
       else{
         result=mysql_store_result(&mysql);
-        if(0!=mysql_num_rows(result))
+        if(0!=mysql_num_rows(result)){
+          //数据库存有记录
           while((row=mysql_fetch_row(result)))
-            if(0==strcmp(name,row[0]))
-              goto a;//如果匹配成功就跳转，否则即为输入错误
-          printf("\t账户名或密码输入错误!!!\n");
+            if(0==strcmp(row[0],name)){
+              printf("\t此用户名已存在，按回车继续!!!\n");
+              mysql_free_result(result);
+              getchar();
+              goto show_menu;
+            }
+        }
+        //用户名不存在，继续输入密码完成注册
+        printf("\t密码:_\b");
+        fgets(passwd,50,stdin);
+        passwd[strlen(passwd)-1]='\0';
+        //构造sql插入语句
+        sql="insert into passwd(number,passwd) values('";
+        strncpy(dest,sql,200);
+        strncat(dest,name,200);
+        strncat(dest,"',",200);
+        strncat(dest,"password('",200);
+        strncat(dest,passwd,200);
+        strncat(dest,"'))",200);
+        //构造成功，开始注册
+        if(mysql_query(&mysql,dest)){
+          //注册失败
+          mysql_free_result(result);
+          fprintf(stderr,"\t服务器故障，注册失败，请稍后再试!!!\n");
+          fprintf(stderr,"\t按回车继续...");
+          getchar();
+          exit(EXIT_FAILURE);
+        }
+        printf("\t注册成功，按回车继续...");
+        getchar();
+        goto show_profile;
       }
-      printf("\t按回车继续...\n");
-      getchar();
-      //回收结果集空间
-      mysql_free_result(result);
     }
-    else{
-      //注册
-    }
+    else if('3'==ch)
+      exit(EXIT_SUCCESS);
   }
-a:mysql_free_result(result);
-  printf("\t登录成功，2秒后自动跳转...\n");
+show_profile:mysql_free_result(result);
   return;
 }
